6ebbebbe3985e7bec33aa14239c110365ca7fa56
Vikash <vikashvarnsolutions@gmail.com>
2026-09-26 16:06:14 +0530

    Update Google Tag Manager ID to GTM-5JDF8FK4

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -79,7 +79,7 @@ export default function RootLayout({ children }) {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MHPT6JPQ');`,
+})(window,document,'script','dataLayer','GTM-5JDF8FK4');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -88,7 +88,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MHPT6JPQ"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5JDF8FK4"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
